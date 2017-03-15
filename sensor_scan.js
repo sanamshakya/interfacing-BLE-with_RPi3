36e962208e2e0e634922ff7af57af1fccefcbfb6
Sanam Shakya <sanam_shakya@yahoo.com>
2017-03-15 17:27:28 +0530

    added the scan log data

--- sensor_scan.js
+++ sensor_scan.js
@@ -8,5 +8,37 @@ noble.on('stateChange', function(state){
       }
 });
 
+noble.on('discover', function(peripheral) {
+    var advertisement = peripheral.advertisement;
+
+    var localName = advertisement.localName;
+    var txPowerLevel = advertisement.txPowerLevel;
+    var manufacturerData = advertisement.manufacturerData;
+    var serviceData = advertisement.serviceData;
+    var serviceUuids = advertisement.serviceUuids;
+
+    if (localName) {
+      console.log('  Local Name        = ' + localName);
+    }
+
+    if (txPowerLevel) {
+      console.log('  TX Power Level    = ' + txPowerLevel);
+    }
+
+    if (manufacturerData) {
+      console.log('  Manufacturer Data = ' + manufacturerData.toString('hex'));
+    }
+
+    if (serviceData) {
+      console.log('  Service Data      = ' + serviceData);
+    }
+
+    if (serviceUuids) {
+      console.log('  Service UUIDs     = ' + serviceUuids);
+    }
+
+    console.log();
 
 
+  }
+});
