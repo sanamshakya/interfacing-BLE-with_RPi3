15340a10418c0b417b1ea958f945293421a59af9
Sanam Shakya <sanam_shakya@yahoo.com>
2017-03-15 11:08:18 +0000

    added the sensor notify data

--- sensor_scan.js
+++ sensor_scan.js
@@ -9,6 +9,15 @@ noble.on('stateChange', function(state){
 });
 
 noble.on('discover', function(peripheral) {
+
+      var localName = peripheral.advertisement.localName;
+
+    // find SensorTag based on local name
+       if (localName && localName.match(/WICED/)) {
+           noble.stopScanning();
+               console.log('Attempting to connect to ' + localName);
+               connectAndSetUpSensorTag(peripheral);
+        }    
     var advertisement = peripheral.advertisement;
 
     var localName = advertisement.localName;
@@ -39,6 +48,53 @@ noble.on('discover', function(peripheral) {
 
     console.log();
 
-
-  }
 });
+
+function connectAndSetUpSensorTag(peripheral) {
+
+    peripheral.connect(function(error) {
+       console.log('Connected to ' + peripheral.advertisement.localName);
+       if (error) {
+          console.log('There was an error connecting ' + error);
+          return;
+       }
+
+var serviceUUIDs = ['739298b687b64984a5dcbdc18b068985'];
+var characteristicUUIDs = ['33ef91133b55413eb553fea1eaada459'];
+
+peripheral.discoverSomeServicesAndCharacteristics(serviceUUIDs, characteristicUUIDs, onServicesAndCharacteristicsDiscovered);
+    });
+  // attach disconnect handler
+    peripheral.once('disconnect', onDisconnect);
+}
+
+function onDisconnect() {
+     console.log('Peripheral disconnected!');
+     noble.startScanning();
+}
+
+function onServicesAndCharacteristicsDiscovered(error, services, characteristics) {
+
+        if (error) {
+            console.log('Error discovering services and characteristics ' + error);
+            return;
+        }
+
+        var characteristic = characteristics[0];
+
+        // subscribe for notifications
+        characteristic.notify(true);
+        
+        // called when notification state changes
+        characteristic.on('notify', function(isNotifying) {
+              if (isNotifying) {
+              console.log('SensorTag remote is ready');
+              }
+         });
+  // called when the data changes
+  characteristic.on('data', onCharacteristicData);
+}
+
+function onCharacteristicData(data, isNotification) {
+        console.log(data);
+}
